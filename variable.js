79907b3c3dfda60b5e71df9a823d80910b974de5
rahmanrizaluddin <114675284+rahmanrizaluddin@users.noreply.github.com>
2023-10-03 12:46:37 +0700

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,8 @@
+const firstName = "Rahman";
+const lastName = "Rizaluddin";
+const age = 20;
+const isMarried = false;
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(isMarried)
